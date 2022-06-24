787d29385164438383a8394c57e66314b9137f07
javier <jjr.agnir@gmail.com>
2022-06-24 15:26:40 +0800

    Create canvas creation input function

--- scripts.js
+++ scripts.js
@@ -1,13 +1,27 @@
 
 
-const container = document.querySelector('#gridContainer')
 
-console.log(container);
+const container = document.querySelector('#gridContainer');
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', getSize);
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.addEventListener('mouseover', () => square.classList.add('hover'));
-    square.addEventListener('mouseout', () => square.classList.remove('hover'));
-    container.appendChild(square);
+function printSquares(size) {
+    for (let i = 0, j = size ** 2; i < j; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mouseover', () => square.classList.add('hover'));
+        /* square.addEventListener('mouseout', () => square.classList.remove('hover')); */
+        container.appendChild(square);
+    }
+}
+
+function getSize() {
+    let number = prompt('Please enter how many squares should fit in the width.');
+    if (Number.isInteger(number) === false || number < 1 || number > 100) {
+        alert('Input must be an integer between 1 and 100.')
+        return;
+    } else {
+        printSquares(number);
+        return;
+    }
 }
