b98503756eb7c5db676ed793b17ea6f2576e2828
javier <jjr.agnir@gmail.com>
2022-06-24 15:51:48 +0800

    Add dynamic pixel amounts in canvas

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@ const container = document.querySelector('#gridContainer');
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', getSize);
 
+// Create the grid given a number of squares
 function printSquares(size) {
     for (let i = 0, j = size ** 2; i < j; i++) {
         let square = document.createElement('div');
+        square.style.width = `${960 / size}px`;
         square.classList.add('square');
         square.addEventListener('mouseover', () => square.classList.add('hover'));
         /* square.addEventListener('mouseout', () => square.classList.remove('hover')); */
@@ -13,6 +15,7 @@ function printSquares(size) {
     }
 }
 
+// Ask the user for input and feed the input into printSquares
 function getSize() {
     let number = parseInt(prompt('Please enter how many squares should fit in the width.'));
     console.log(number);
@@ -20,6 +23,7 @@ function getSize() {
         alert('Input must be an integer between 1 and 100.')
         return;
     } else {
+        container.style.gridTemplateColumns = `repeat(${number}, 1fr)`
         printSquares(number);
         return;
     }
